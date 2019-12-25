52e6c7c56aa208995a6e122dc4de98295a217d8a
丁亚辉 <2294211995@qq.com>
2019-12-25 15:11:02 +0800

    first

---
+++ src/Print/PrintErrorAndDebug.java
@@ -0,0 +1,8 @@
+package Print;
+
+public class PrintErrorAndDebug {
+    public static void main(String[] args) {
+        System.out.println("main()开始运行");
+        System.err.println("发生了一个错误");
+    }
+}
